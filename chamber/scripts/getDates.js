d5ec2b3e2d528a509e1f15ba6e6e8b589b267c66
mkatopola <moffatkatopola@gmail.com>
2024-06-24 00:54:37 +0200

    Chamber home page

---
+++ chamber/scripts/getDates.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var currentYear = new Date().getFullYear();
+
+    // var footerCopyright = document.querySelector("footer p:first-child");
+    // footerCopyright.innerHTML = "&copy;" + currentYear + " Moffat Katopola | Malawi <span class='flag-icon'><img src='images/flag-icon.png' alt='Malawi Flag'></span>";
+
+    var lastModified = document.lastModified;
+    var lastModifiedParagraph = document.querySelector("#lastModified");
+    lastModifiedParagraph.textContent = "Last Modification: " + lastModified;
+});
+
