215bb5276afea6ef27c730110935e5239713503b
mkatopola <moffatkatopola@gmail.com>
2024-06-29 21:53:13 +0200

    Home Page—Responsive Images

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,10 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
     var currentYear = new Date().getFullYear();
 
     var footerCopyright = document.querySelector("footer p:first-child");
-    footerCopyright.innerHTML = "&copy;" + currentYear + " Moffat Katopola | Malawi <span class='flag-icon'><img src='images/flag-icon.png' alt='Malawi Flag'></span>";
+    footerCopyright.innerHTML = "&copy;" + currentYear + " Moffat Katopola | Malawi <span class='flag-icon'><img src='images/flag-icon.webp' alt='Malawi Flag'></span>";
 
     var lastModified = document.lastModified;
     var lastModifiedParagraph = document.querySelector("#lastModified");
     lastModifiedParagraph.textContent = "Last Modification: " + lastModified;
 });
 
+// 1️⃣ Initialize display element variable
+const visitsDisplay = document.querySelector(".visits");
+
+// 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+// 4️⃣ increment the number of visits by one.
+numVisits++;
+
+// 5️⃣ store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
+
+// 💡A client can view the localStorage data using the Applications panel in the browsers's DevTools - check it out on any major site.
